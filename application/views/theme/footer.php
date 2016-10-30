7717e81fc1c2cabcf5adf34b842f73714410234a
dpapanikolaou <dionysis_172@hotmail.com>
2016-10-30 16:48:01 +0200

    login controller & model

---
+++ application/views/theme/footer.php
@@ -0,0 +1,3 @@
+<footer class="modal-footer">
+    <p>&copy; <?php echo date('Y') ?> dpapanik :P</p>
+</footer>
---
+++ application/views/theme/head.php
@@ -0,0 +1,12 @@
+<title><?php echo $title ?></title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=yes"/>
+<meta name="HandheldFriendly" content="true"/>
+<meta name="apple-touch-fullscreen" content="yes"/>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/bootstrap.css"/>
+<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/bootstrap-theme.css"/>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/jquery-3.1.1.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/bootstrap.js"></script>
+<script src="//code.highcharts.com/highcharts.js"></script>
+<script src="//code.highcharts.com/modules/exporting.js"></script>
