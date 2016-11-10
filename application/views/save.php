ec9e98031a50a5ee88ddcc73f2e85ea6e90e8787
dpapanikolaou <dionysis_172@hotmail.com>
2016-11-10 23:25:10 +0200

    save data into db

--- application/views/save.php
+++ application/views/save.php
@@ -7,7 +7,7 @@
 <body>
 <div class="container">
     <?php $this->load->view('theme/header'); ?>
-    <?php echo form_open('save', array('class' => 'form-inline')); ?>
+    <?php echo form_open('save'); ?>
     <h1>Insert Data Into Database</h1>
 
     <?php if (isset($message)) : ?>
@@ -15,27 +15,34 @@
     <?php endif; ?>
 
     <div class="form-group">
-        <?php echo form_label('Date From :', 'date_from'); ?> <?php echo form_error('date_from'); ?>
-        <?php echo form_input(array('id' => 'date_from', 'name' => 'date_from')); ?><br/>
+        <?php echo form_label('Date From :', 'date_from'); ?><?php echo form_error('date_from'); ?>
+        <?php echo form_input(array('id' => 'date_from', 'name' => 'date_from')); ?>
     </div>
 
-    <?php echo form_label('Date To :'); ?> <?php echo form_error('date_to'); ?><br/>
-    <?php echo form_input(array('id' => 'date_to', 'name' => 'date_to')); ?><br/>
+    <div class="form-group">
+        <?php echo form_label('Date To :'); ?><?php echo form_error('date_to'); ?>
+        <?php echo form_input(array('id' => 'date_to', 'name' => 'date_to')); ?>
+    </div>
 
-    <?php echo form_label('Liters :'); ?> <?php echo form_error('liters'); ?><br/>
-    <?php echo form_input(array('id' => 'liters', 'name' => 'liters')); ?>
-    <br/>
-
-    <?php echo form_label('Km :'); ?> <?php echo form_error('km'); ?><br/>
-    <?php echo form_input(array('id' => 'km', 'name' => 'km')); ?>
-    <br/>
-
-    <?php echo form_label('Money :'); ?> <?php echo form_error('money'); ?><br/>
-    <?php echo form_input(array('id' => 'money', 'name' => 'money')); ?>
-    <br/>
+    <div class="form-group">
+        <?php echo form_label('Liters :'); ?><?php echo form_error('liters'); ?>
+        <?php echo form_input(array('id' => 'liters', 'name' => 'liters')); ?>
+    </div>
 
 
-    <?php echo form_submit(array('id' => 'submit', 'value' => 'Submit')); ?>
+    <div class="form-group">
+        <?php echo form_label('Km :'); ?><?php echo form_error('km'); ?>
+        <?php echo form_input(array('id' => 'km', 'name' => 'km')); ?>
+    </div>
+
+
+    <div class="form-group">
+        <?php echo form_label('Money :'); ?><?php echo form_error('money'); ?>
+        <?php echo form_input(array('id' => 'money', 'name' => 'money')); ?>
+    </div>
+
+
+    <?php echo form_submit(array('id' => 'submit', 'class' => 'btn btn-success', 'value' => 'Submit')); ?>
     <?php echo form_close(); ?><br/>
 
     <?php $this->load->view('theme/footer'); ?>
